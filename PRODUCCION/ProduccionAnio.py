296a67ca23d2831429c4dfd6c88ca00d97fbd152
MARIA JOSE SIMON <mariajose.simon@ikasle.egibide.org>
2021-04-25 15:45:48 +0200

    Dataframe

--- PRODUCCION/ProduccionAnio.py
+++ PRODUCCION/ProduccionAnio.py
@@ -1,22 +1,24 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-def produccionAnual(db):
+'''https://www.analyticslane.com/2020/11/16/pandas-como-crear-un-dataframe-vacio-y-agregar-datos/'''
 
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+def produccionAnual(db):
     consulta = "select NombreProducto as 'Nombre Producto', COUNT(cantidadProducida) as 'Cantidad producida', year(FechaOrden) as 'Año' FROM ordenproduccion, productoscreados where ordenproduccion.idProductoFinal = productoscreados.idProducto group by idProductoFinal, year(FechaOrden)"
     cursor = db.cursor()
     cursor.execute(consulta)
 
     resultadoconsulta = cursor.fetchall()
 
-
-    datos =[[]]
-
+    datos = [[]]
 
     for i in resultadoconsulta:
         datos.append([i[0], i[1], i[2]])
 
+    df = pd.DataFrame(columns=['Nombre', 'Anio', 'Cantidad'])
 
+    for i in resultadoconsulta:
+        df = df.append({'Nombre': i[0], 'Anio': i[2], 'Cantidad': i[1]}, ignore_index=True)
 
-    df=pd.DataFrame(datos,columns=["Producto","cantidad","Anio"])
-    df.plot(x=["Producto","Anio"], y=["cantidad"], kind="bar",figsize=(9,8))
-    plt.show()
